7ee2f35db45c4d38971a3abc3d23d7535fe11023
Saber <35178886+AnthonyTriguero@users.noreply.github.com>
2020-06-29 21:59:42 -0500

    new index.js

--- Chara-bot.v1/src/index.js
+++ Chara-bot.v1/src/index.js
@@ -2,17 +2,35 @@
 const { Client } = require ('discord.js');
 const client = new Client();
 
+function presence(){
+  client.user.setPresence({
+     status: "online",
+     activity: {
+        name: "Undertale",
+        type: "PLAYING"
+     }
+  });
+}
+
+client.on('ready' ,() => {
+  console.log('Bot Listo On fire ');
+  presence();
+});
+
+
 client.on('message', msg => {
     if (msg.content === '&Estado') {
-      msg.reply('100% NUEVO HOST');
+      msg.reply('NUEVO HOST');
     }
   });
 
-client.on('ready' ,() => {
-    console.log('Bot Listo On fire ');
-});
 
 client.on('message', message  => {
     console.log(message)
 });
-client.login();
+
+client.on("guildMemberAdd", miembro =>{
+  var Canal = client.channels.cache.get(("ID Canal"));
+  Canal.send("Bienvenido <@" + miembro.id + "> al servidor.\n\nEsperemos que la pases bien :). :stuck_out_tongue_winking_eye: ");
+});
+client.login("token");
